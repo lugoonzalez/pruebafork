d99d7c407c99e84aac575c0bedef2087e8e2bb2e
lugoonzalez <lugoonzalez@gmail.com>
2019-11-15 10:25:16 -0300

    Cambios de Lucia

---
+++ PruebaLucia/Cambios.java
@@ -0,0 +1,25 @@
+/**
+ * Cambios
+ */
+public class Cambios {
+
+    private int edad;
+
+    private String nombre;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pruebafork
+# probandForkConGit
